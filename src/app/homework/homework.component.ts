c4e4f6bf07b9630bbaf49d0139aff433758bf470
Saltanova Zhanna <nedikovazhanna@gmail.com>
2023-03-23 19:51:53 +0200

    first commit

--- src/app/homework/homework.component.ts
+++ src/app/homework/homework.component.ts
@@ -1,66 +1,108 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,  OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-homework',
-  templateUrl: './homework.component.html',
+  templateUrl: './homework.component.html', 
   styleUrls: ['./homework.component.scss']
 })
 export class HomeworkComponent implements OnInit {
-  public badContent: string[] = [];
-  public badWord: string = '';
-  public cenzorText: string = '';
-  public textHere = 'text here..';
-  public wordHere = 'word here..'
-  public isEmpty = false;
-  public isEmptyCenzor = false;
+
+    public isBtn = true;
+    public inputWrong1 = false;
+    public inputWrong2 = false;
+    public inputWrong3 = false;
+    public userIndex!: number;
+    public user = {
+      login: '',
+      password: '',
+      email: ''
+    };
+    public regExp = {
+      login: /^[\w_\-.]{4,20}$/,
+      password: /^[\w_\-.\S]{4,20}$/,
+      email: /^[a-z0-9_\-.]+@[a-z.]+\.[a-z]+$/
+    }
+    public usersArray: Array<{
+      login: string, 
+      password: string, 
+      email: string
+    }> = [];
 
 
-  private re = /\w/g;
+  
   constructor() { }
-
 
   ngOnInit(): void {
   }
 
-  addBadWord(): void {
-    if (this.badWord.trim()) {
-      this.badContent.push(this.badWord.trim());
-      this.badWord = '';
-      this.wordHere = 'word here..';
-      this.isEmpty = false;
 
+
+  addUser(): void {
+    if(!this.checkInputs()) {
+      return;
+    }
+    this.usersArray.push(this.createNewUser());
+    this.clearInputs();
+  }
+
+  checkInputs(): boolean {
+    if(!this.regExp.login.test(this.user.login)) {
+      this.inputWrong1 = true;
+      return false;
     }
     else {
-      this.wordHere = 'Please write a word!';
-      this.isEmpty = true;
-
+      this.inputWrong1 = false;
     }
-  }
-  removeBadWords(): void {
-    this.badContent.length = 0;
-    this.cenzorText = '';
-    this.isEmpty = false;
-    this.isEmptyCenzor = false;
-    this.wordHere = 'word here..';
-    this.textHere = `text here.. `;
-  }
-
-  cenzor(): void {
-
-    if (this.cenzorText.length > 0) {
-      const textCenzor: string[] = this.cenzorText.split(" ");
-      this.cenzorText = textCenzor.map(elem => this.badContent.includes(elem) ? elem.replace(this.re, '*') : elem).join(' ');
-      this.textHere = `text here.. `;
-      this.isEmptyCenzor = false;
-
+    if(!this.regExp.password.test(this.user.password)) {
+      this.inputWrong2 = true;
+      return false;
     }
     else {
-      this.textHere = `Please write a text!`;
-      this.isEmptyCenzor = true;
+      this.inputWrong2 = false;
     }
+    if(!this.regExp.email.test(this.user.email)) {
+      this.inputWrong3 = true;
+      return false;
+    }
+    else {
+      this.inputWrong3 = false;
+    }
+    return true;
+  }
+  createNewUser() {
+    return {
+      login: this.user.login,
+      password: this.user.password,
+      email: this.user.email
+    };
+  }
+  clearInputs():void {
+    this.user.login = '';
+    this.user.password = '';
+    this.user.email = '';
+  }
+  deleteUser(index: number): void {
+    this.usersArray.splice(index, 1);
+  }
+  editUser(index: number): void {
+    this.isBtn = false;
+    this.user.login = this.usersArray[index].login;
+    this.user.password = this.usersArray[index].password;
+    this.user.email = this.usersArray[index].email;
+    this.userIndex = index;
+  }
+  saveEditUser(): void {
+    if(!this.checkInputs()) {
+      return;
+    }
+    this.usersArray[this.userIndex] = this.createNewUser();
+    this.isBtn = true;
+    this.clearInputs();
+  }
+
   }
 
 
-}
+
 
 
