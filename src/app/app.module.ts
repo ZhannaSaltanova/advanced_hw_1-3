bd714a633e84383bab5adf32788265ac47ed4e8d
Saltanova Zhanna <nedikovazhanna@gmail.com>
2023-03-28 13:00:24 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeworkComponent } from './homework/homework.component';
+import { Homework03Component } from './homework03/homework03.component';
+import { TableComponent } from './homework03/table/table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeworkComponent
+    HomeworkComponent,
+    Homework03Component,
+    TableComponent
   ],
   imports: [
     BrowserModule,
